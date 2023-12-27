c96ddf0b78274781f31a7cc2f9a4fdca0644bec6
yazzfr <154938656+yazzfr@users.noreply.github.com>
2023-12-26 20:27:08 -0600

    Update ezloaderbyyazz.js

--- ezloaderbyyazz.js
+++ ezloaderbyyazz.js
@@ -1,7 +1,1 @@
-javascript: (() => {
-var banner = document.createElement("div"); banner.textContent = "External Mod Loader"; banner.style.backgroundColor = "black"; banner.style.color = "white"; banner.style.padding = "10px"; banner.style.textAlign = "center"; topbar.appendChild(banner);
-var url = "RAW URL";
-fetch(url)
-    .then(response => response.text())
-    .then(data => eval(data));
-})();
+javascript: var banner = document.createElement("div"); banner.textContent = "Ez Mod Loader"; banner.style.backgroundColor = "black"; banner.style.color = "white"; banner.style.padding = "10px"; banner.style.textAlign = "center"; topbar.appendChild(banner); var url = "RAW URL HERE"; fetch(url).then(response => response.text()).then(data => eval(data));
