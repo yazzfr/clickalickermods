64502d4e5ad17057a48724b2a7950b0ef5163693
yazzfr <154938656+yazzfr@users.noreply.github.com>
2023-12-26 20:22:52 -0600

    Update YazzsExternalModLoader.js

--- YazzsExternalModLoader.js
+++ YazzsExternalModLoader.js
@@ -1,4 +1,7 @@
-javascript: (() => {var url = prompt("Enter the URL:");
-    fetch(url)
-        .then(response => response.text())
-        .then(data => eval(data));})();
+javascript: (() => {
+var banner = document.createElement("div"); banner.textContent = "External Mod Loader"; banner.style.backgroundColor = "black"; banner.style.color = "white"; banner.style.padding = "10px"; banner.style.textAlign = "center"; topbar.appendChild(banner);
+var url = "RAW URL";
+fetch(url)
+    .then(response => response.text())
+    .then(data => eval(data));
+})();
